18a859c288216c4d206e2857cbb8fba6ab0d9225
bill_wy <11372349@qq.com>
2024-06-14 10:35:23 +0800

    1. 修复nexus仓库上传Docker镜像前删除旧有镜像不干净的BUG

--- script/pipeline-stages/chain/onBeforePushDockerImage.sh
+++ script/pipeline-stages/chain/onBeforePushDockerImage.sh
@@ -70,19 +70,28 @@ function onBeforePushDockerImage_nexus() {
 
   local l_imageName
   local l_imageVersion
+  local l_versionList
+  local l_versionItem
 
   l_imageName="${l_image%:*}"
   l_imageVersion="${l_image##*:}"
 
-  info "在${l_dockerRepoType}类型的docker仓库中查找现存的${l_imageName}:${l_imageVersion}镜像..."
-  queryNexusComponentId "${l_dockerRepoName%%:*}" "${l_dockerRepoWebPort}" "${l_dockerRepoInstanceName}" "${l_imageName}" "${l_imageVersion}"
-  if [ "${gDefaultRetVal}" ];then
-    info "找到了目标镜像，开始清除..."
-    deleteNexusComponentById "${l_dockerRepoName%%:*}" "${l_dockerRepoWebPort}" "${gDefaultRetVal}"
-    info "目标镜像清除成功"
-  else
-    warn "目标镜像不存在"
-  fi
+  l_versionList=("${l_imageVersion}" "${l_imageVersion}-linux-amd64" "${l_imageVersion}-linux-arm64")
+
+  # shellcheck disable=SC2068
+  for l_versionItem in ${l_versionList[@]};do
+
+    info "在${l_dockerRepoType}类型的docker仓库中查找现存的${l_imageName}:${l_versionItem}镜像..."
+    queryNexusComponentId "${l_dockerRepoName%%:*}" "${l_dockerRepoWebPort}" "${l_dockerRepoInstanceName}" "${l_imageName}" "${l_versionItem}"
+    if [ "${gDefaultRetVal}" ];then
+      info "找到了目标镜像，开始清除..."
+      deleteNexusComponentById "${l_dockerRepoName%%:*}" "${l_dockerRepoWebPort}" "${gDefaultRetVal}"
+      info "目标镜像清除成功"
+    else
+      warn "目标镜像不存在"
+    fi
+
+  done
 
   gDefaultRetVal="true|true"
 }
