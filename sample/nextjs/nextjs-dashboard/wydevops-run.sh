9f1e39970b6da1e43dc6a2bb07cc56f82061b598
bill_wy <11372349@qq.com>
2024-12-20 22:22:24 +0800

    1. 优化配置文件

--- sample/nextjs/nextjs-dashboard/wydevops-run.sh
+++ sample/nextjs/nextjs-dashboard/wydevops-run.sh
@@ -12,7 +12,7 @@ bash "${_SCRIPT_ROOT_DIR}/wydevops.sh" -e -d -m \
 -B single \
 -I /d/cachedImage \
 -L nextjs \
--S deploy \
+-S build,docker,chart,package,deploy \
 -M local \
 -T true \
 -W "${_SCRIPT_ROOT_DIR}" \
