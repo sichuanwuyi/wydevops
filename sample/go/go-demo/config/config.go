1fd61619f5fc18788ad1ef47c41ac66be0e173c8
bill_wy <11372349@qq.com>
2025-05-02 11:59:43 +0800

    1. java项目的params-mapping-in-yaml-file.config文件中api-url-prefix参数名称变更为gateway.url-prefix 1. go项目的params-mapping-in-yaml-file.config文件中app.api-url-prefix参数名称变更为app.gateway.url-prefix

--- sample/go/go-demo/config/config.go
+++ sample/go/go-demo/config/config.go
@@ -10,12 +10,16 @@ type Mysql struct {
 	Password  string `mapstructure: "password" json:"password" yaml:"password"`
 }
 
+type Gateway struct {
+	url-prefix string `mapstructure: "url-prefix" json:"url-prefix" yaml:"url-prefix"`
+}
+
 type App struct {
 	Env     string `mapstructure: "env" json:"env" yaml:"env"`
 	Port    string `mapstructure: "port" json:"port" yaml:"port"`
 	AppName string `mapstructure: "appName" json:"appName" yaml:"appName"`
 	AppUrl  string `mapstructure: "appUrl" json:"appUrl" yaml:"appUrl"`
-    ApiUrlPrefix  string `mapstructure: "apiUrlPrefix" json:"apiUrlPrefix" yaml:"apiUrlPrefix"`
+    Gateway Gateway `mapstructure: "gateway" json:"gateway" yaml:"gateway"`
 	Version  string `mapstructure: "version" json:"version" yaml:"version"`
 	Mysql Mysql `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
 }
