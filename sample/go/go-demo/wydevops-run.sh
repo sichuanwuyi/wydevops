262dbfd8dc72d9b991d1705dd3351040986e6563
bill_wy <11372349@qq.com>
2026-03-10 13:49:09 +0800

    1. 将所有文件的换行符改为了linux风格"\n"。

--- sample/go/go-demo/wydevops-run.sh
+++ sample/go/go-demo/wydevops-run.sh
@@ -8,11 +8,13 @@ BBlue='\033[1;34m'
 _WYDEVOPS_HOME="${WYDEVOPS_HOME:=$HOME/.wydevops}"
 echo -e "${BBlue}_WYDEVOPS_HOME=${_WYDEVOPS_HOME}${Color_Off}"
 # The shared local directory where the scripts will be cloned.
-_SCRIPTS_PROJECT_DIR="${_WYDEVOPS_HOME}/project"
+_SCRIPTS_PROJECT_DIR="${_WYDEVOPS_HOME}/wydevops"
 echo -e "${BBlue}_SCRIPTS_PROJECT_DIR=${_SCRIPTS_PROJECT_DIR}${Color_Off}"
 _SCRIPTS_ROOT_DIR="${_SCRIPTS_PROJECT_DIR}/script"
 echo -e "${BBlue}_SCRIPTS_ROOT_DIR=${_SCRIPTS_ROOT_DIR}${Color_Off}"
 
+_selfRootDir="${_SCRIPTS_ROOT_DIR}"
+source "${_SCRIPTS_ROOT_DIR}/helper/yaml-helper.sh"
 source "${_SCRIPTS_ROOT_DIR}/wydevops-update.sh"
 source "${_SCRIPTS_ROOT_DIR}/helper/path-helper.sh"
 
