25f62bea4dad617ad935d7255c6c60bd6d009fca
bill_wy <11372349@qq.com>
2026-03-20 15:39:24 +0800

    1. 添加传入参数解密机制

--- script/pipeline-stages/common/secret-extend-point.sh
+++ script/pipeline-stages/common/secret-extend-point.sh
@@ -6,7 +6,6 @@ function decodeSecretInfo_ex() {
   local l_paramName="$1"
   local l_paramValue="$2"
 
-  echo "----------decodeSecretInfo_ex-----------"
   warn "common.secret.extend.point.decoded.secret.info" "${l_paramName}#${l_paramValue}"
 
   gDefaultRetVal="${l_paramValue}"
