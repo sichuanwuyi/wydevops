2a6aa4b8ec96fd370470ddb3141a10b4cea1d8f7
bill_wy <11372349@qq.com>
2026-03-24 15:11:47 +0800

    1. 新增自动免密配置

--- script/helper/ssh-helper.sh
+++ script/helper/ssh-helper.sh
@@ -15,22 +15,22 @@ function tryConnectByPasswordless() {
   l_idRSAFile="${HOME}/.ssh/id_rsa"
   # shellcheck disable=SC2088
   if [ ! -f "${l_idRSAFile}" ];then
-    info "ssh.helper.execute.command.ssh.keygen"
+    info "ssh.helper.execute.command.ssh-keygen"
     ssh-keygen -t rsa -b 4096 -f "${l_idRSAFile}" -N "" -C "wydevops@wydevops.com"
     if [ "$?" -ne 0 ];then
-      warn "ssh.helper.execute.command.failed" "unknown"
+      warn "ssh.helper.execute.ssh-keygen.failed" "unknown"
       gDefaultRetVal="false"
       return
     fi
   fi
 
-  info "ssh.helper.execute.command.ssh.copy.id" "${l_host}" "-n"
+  info "ssh.helper.execute.command.ssh-copy-id" "${l_host}" "-n"
   l_result=$(ssh-copy-id -o "BatchMode=yes" "${l_user}@${l_host}" 2>&1)
   if [ "$?" -ne 0 ];then
-    warn "ssh.helper.execute.command.failed" "\n${l_result}" "*"
+    warn "ssh.helper.execute.ssh-copy-id.failed" "\n${l_result}" "*"
     gDefaultRetVal="false"
     return
   fi
-  warn "ssh.helper.execute.command.success" "" "*"
+  warn "ssh.helper.execute.ssh-copy-id.success" "" "*"
 
 }
