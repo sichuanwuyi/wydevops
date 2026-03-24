5139a086108283282ddeb725dd8d2a590acc0ccd
bill_wy <11372349@qq.com>
2026-03-24 14:02:29 +0800

    1. 新增自动免密配置

--- script/helper/ssh-helper.sh
+++ script/helper/ssh-helper.sh
@@ -12,20 +12,20 @@ function tryConnectByPasswordless() {
 
   # shellcheck disable=SC2088
   if [ ! -f "~/.ssh/id_rsa.pub" ];then
-    info "ssh.helper.execute.command.ssh.keygen" "" "-n"
+    info "ssh.helper.execute.command.ssh.keygen" ""
     ssh-keygen -t rsa -b 4096 -C "wydevops@wydevops.com"
     if [ "$?" -ne 0 ];then
-      warn "ssh.helper.execute.command.failed" "timeout" "*"
+      warn "ssh.helper.execute.command.failed" "unknown"
       gDefaultRetVal="false"
       return
     fi
-    warn "ssh.helper.execute.command.success" "" "*"
+    warn "ssh.helper.execute.command.success"
   fi
 
   info "ssh.helper.execute.command.ssh.copy.id" "${l_host}" "-n"
   l_result=$(ssh-copy-id "${l_user}@${l_host}" 2>&1)
   if [ "$?" -ne 0 ];then
-    warn "ssh.helper.execute.command.failed" "${l_result}" "*"
+    warn "ssh.helper.execute.command.failed" "\n${l_result}" "*"
     gDefaultRetVal="false"
     return
   fi
