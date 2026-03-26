e8a44c1812c7339f09699e774323db630ba60ef2
bill_wy <11372349@qq.com>
2026-03-26 09:47:22 +0800

    1. 排查docker部署模式下scp命令执行失败的原因

--- script/templates/deploy/go/docker-run-generator.sh
+++ script/templates/deploy/go/docker-run-generator.sh
@@ -20,6 +20,7 @@ function generateDockerRunShellFile() {
   local l_port
   local l_exposePorts
   local l_mainImage
+  local l_workDirInContainer
 
   #获取需要暴露的端口号。
   readParam "${gCiCdYamlFile}" "docker.exposePorts"
@@ -31,9 +32,13 @@ function generateDockerRunShellFile() {
   done
 
   #读取第一个镜像。
-  l_mainImage=${l_images%%,*}
+  l_mainImage="${l_images%%,*}"
   #删除镜像名称中可能存在的_base后缀(docker发布模式只能使用single模式构建的镜像)。
   l_mainImage="${l_mainImage//-base:/:}"
+
+  #获取配置文件挂载路径。
+  readParam "${gCiCdYamlFile}" "docker.workDir"
+  l_workDirInContainer="${gDefaultRetVal}"
 
   #在gBuildPath路径中输出docker-run.sh文件。
   if [ "${l_repoName}" ];then
@@ -41,16 +46,16 @@ function generateDockerRunShellFile() {
   # shellcheck disable=SC2027
   echo \"echo ${l_password} | docker login ${l_repoName} -u ${l_account} --password-stdin\"
   echo \"${l_password}\" | docker login \"${l_repoName}\" -u \"${l_account}\" --password-stdin
-  echo \"docker rm -f ${l_chartName}\"
-  docker rm -f \"${l_chartName}\"
-  echo \"docker run -d ${l_exposePorts:1} --name ${l_chartName} ${l_repoName}/${l_mainImage}\"
-  docker run -d ${l_exposePorts:1} --name ${l_chartName} ${l_repoName}/${l_mainImage}" > "${gBuildPath}/docker-run.sh"
+  echo \"docker rm -f ${l_mainImage}\"
+  docker rm -f \"${l_mainImage}\"
+  echo \"docker run -d --platform \$1 ${l_exposePorts:1} -v ${l_remoteDir}/config:${l_workDirInContainer}/config --name ${l_chartName} ${l_repoName}/${l_mainImage}\"
+  docker run -d --platform \$1 ${l_exposePorts:1} -v ${l_remoteDir}/config:${l_workDirInContainer}/config --name ${l_chartName} ${l_repoName}/${l_mainImage}" > "${gBuildPath}/docker-run.sh"
   else
   echo "#!/usr/bin/env bash
-  echo \"docker rm -f ${l_chartName}\"
-  docker rm -f \"${l_chartName}\"
-  echo \"docker run -d ${l_exposePorts:1} --name ${l_chartName} ${l_mainImage}\"
-  docker run -d ${l_exposePorts:1} --name ${l_chartName} ${l_mainImage}" > "${gBuildPath}/docker-run.sh"
+  echo \"docker rm -f ${l_mainImage}\"
+  docker rm -f \"${l_mainImage}\"
+  echo \"docker run -d --platform \$1 ${l_exposePorts:1} -v ${l_remoteDir}/config:${l_workDirInContainer}/config --name ${l_chartName} ${l_mainImage}\"
+  docker run -d --platform \$1 ${l_exposePorts:1} -v ${l_remoteDir}/config:${l_workDirInContainer}/config --name ${l_chartName} ${l_mainImage}" > "${gBuildPath}/docker-run.sh"
   fi
 }
 
